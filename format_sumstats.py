66d06e02780a205a6f3ed479795f6dbb00d67875
Ran Cui <cranmail@gmail.com>
2019-09-09 09:32:09 -0400

    Added script to munge SAIGE summary statistics

--- format_sumstats.py
+++ format_sumstats.py
@@ -14,8 +14,9 @@ def formatBOLTss(ss_fname,N,output_prefix,path_to_ldsc):
     Use 'CHISQ_LINREG' as the chisq statistics
 
     Resulting formatted summary statistics should have ['SNP','A1','A2','Z','N']
+
+    ALLELE0 is the reference allele, while ALLELE1 is the effect allele.
     '''
-    # remove SNP column
     # use munge_sumstats to format sumstats
     subprocess.call(['python',path_to_ldsc+'munge_sumstats.py',
     '--sumstats',ss_fname,
@@ -28,6 +29,30 @@ def formatBOLTss(ss_fname,N,output_prefix,path_to_ldsc):
     '--ignore','SNP'])
     return
 
+def formatSAIGEss(ss_fname,N,output_prefix,path_to_ldsc):
+    ''' 
+    Format SAIGE summary statistics files.
+
+    Such file should have columns (in order): 'v', 'CHR', 'POS', 'SNPID', 'Allele1', 'Allele2', 'AC_Allele2',
+    'AF_Allele2', 'N', 'BETA', 'SE', 'Tstat', 'p.value', 'p.value.NA',
+    'Is.SPA.converge', 'varT', 'varTstar', 'N.Cases', 'N.Controls',
+    'AF.Cases', 'AF.Controls'
+
+    Use p.value as the --p flag input
+
+    Allele1 is the reference allele, while Allele2 is the effect allele
+    '''
+    subprocess.call(['python',path_to_ldsc+'munge_sumstats.py',
+    '--sumstats',ss_fname,
+    '--N',N,
+    '--out',output_prefix,
+    '--p','p.value',
+    '--a1','Allele2',
+    '--a2','Allele1',
+    '--snp','v',
+    '--ignore','SNPID'])
+    return
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ss-fname')
@@ -35,9 +60,10 @@ if __name__=='__main__':
     parser.add_argument('--output-prefix')
     parser.add_argument('--path-to-ldsc')
     parser.add_argument('--bolt',action='store_true')
+    parser.add_argument('--saige',action='store_true')
     args = parser.parse_args()
 
     if args.bolt:
         formatBOLTss(args.ss_fname,args.N,args.output_prefix,args.path_to_ldsc)
-
-    
+    elif args.saige:
+        formatSAIGEss(args.ss_fname,args.N,args.output_prefix,args.path_to_ldsc) 
